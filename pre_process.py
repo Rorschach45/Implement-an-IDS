d484ffde32fc49281c344d6a934024a2a267df14
alighorbani <alighorbani29@gmail.com>
2018-07-29 16:12:55 +0430

    encoding

--- pre_process.py
+++ pre_process.py
@@ -11,4 +11,9 @@ train_data[3]=train_data[3].astype('category').cat.codes
 
 print(train_data.head())
 print(train_data.shape)
+reduced_attacks={"normal": "normal","neptune":"dos", "warezclient":"r2l", "ipsweep":"probe", "portsweep":"probe", "teardrop":"dos", "nmap":"probe",
+ "satan":"probe", "smurf":"dos", "pod":"dos", "back":"dos","guess_passwd":"r2l","ftp_write":"r2l","multihop":"r2l","rootkit":"u2r","buffer_overflow":"u2r","imap":"r2l","warezmaster":"r2l","phf":"r2l","land":"dos",
+                 "loadmodule":"u2r","spy":"r2l"}
+train_data[41].replace(reduced_attacks,inplace=True)
+
 print (train_data[41].unique())
