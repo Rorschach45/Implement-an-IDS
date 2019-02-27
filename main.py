564d824466a4e40fb33f394bf5f21a4f7115982c
alighorbani <alighorbani29@gmail.com>
2019-02-27 12:16:51 +0330

    new data added

--- main.py
+++ main.py
@@ -1,39 +1,37 @@
 import weka.core.jvm as jvm
-from weka.classifiers import Classifier,MultipleClassifiersCombiner,PredictionOutput
+from weka.classifiers import Classifier, MultipleClassifiersCombiner, PredictionOutput
 from weka.classifiers import Evaluation
 from weka.core.classes import Random
 from weka.core.converters import Loader
 import gc
 
-jvm.start()
-def vote_classifier_train(dicrectory,nameOfDataSet,flag):
+
+def vote_classifier_train(dicrectory, nameOfDataSet, flag):
     loader = Loader(classname="weka.core.converters.CSVLoader")
     data = loader.load_file(dicrectory)
     data.class_is_last()
-    meta = MultipleClassifiersCombiner(classname="weka.classifiers.meta.Vote"
-       , options=['-S', '1', '-B', 'weka.classifiers.trees.J48 -C 0.25 -M 2', '-B',
-                  'weka.classifiers.trees.RandomTree -K 6 -M 1.0 -V 0.001 -S 1',
-                  '-B',
-                  'weka.classifiers.meta.Bagging -P 100 -S 1 -num-slots 1 -I 10 -W weka.classifiers.trees.REPTree -- '
-                  '-M 2 -V 0.001 -N 3 -S 1 -L -1 -I 0.0',
-                  '-B',
-                  'weka.classifiers.meta.AdaBoostM1 -P 100 -S 1 -I 10 -W weka.classifiers.trees.DecisionStump',
-                  '-B',
-                  'weka.classifiers.meta.Bagging -P 100 -S 1 -num-slots 1 -I 10 -W weka.classifiers.trees.REPTree -- '
-                  '-M 2 -V 0.001 -N 3 -S 1 -L -1 -I 0.0', '-B',
-                  'weka.classifiers.bayes.NaiveBayes ', '-R', 'AVG'])
-
+    meta = MultipleClassifiersCombiner(classname="weka.classifiers.meta.Vote",
+                                       options=['-S', '1', '-B', 'weka.classifiers.trees.J48 -C 0.25 -M 2',
+                                                '-B', 'weka.classifiers.trees.RandomTree -K 6 -M 1.0 -V 0.001 -S 1',
+                                                '-B',
+                                                'weka.classifiers.meta.Bagging -P 100 -S 1 -num-slots 1 -I 10 -W weka.classifiers.trees.REPTree -- '
+                                                '-M 2 -V 0.001 -N 3 -S 1 -L -1 -I 0.0', '-B',
+                                                'weka.classifiers.meta.AdaBoostM1 -P 100 -S 1 -I 10 -W weka.classifiers.trees.DecisionStump',
+                                                '-B',
+                                                'weka.classifiers.meta.Bagging -P 100 -S 1 -num-slots 1 -I 10 -W weka.classifiers.trees.REPTree -- '
+                                                '-M 2 -V 0.001 -N 3 -S 1 -L -1 -I 0.0', '-B',
+                                                'weka.classifiers.bayes.NaiveBayes ', '-R', 'AVG'])
     eval = Evaluation(data)
     pout = PredictionOutput(classname="weka.classifiers.evaluation.output.prediction.PlainText")
     if flag:
         eval.crossvalidate_model(meta, data, 10, Random(1), pout)
     else:
-        eval.evaluate_train_test_split(meta,data,80.0,Random(1),pout)
+        eval.evaluate_train_test_split(meta, data, 80.0, Random(1), pout)
     gc.collect()
-    print_and_save('Proposed model',flag,nameOfDataSet,eval)
+    print_and_save('Proposed model', flag, nameOfDataSet, eval)
 
 
-def j48(dicrectory,nameOfDataSet,flag):
+def j48(dicrectory, nameOfDataSet, flag):
     loader = Loader(classname="weka.core.converters.CSVLoader")
     data = loader.load_file(dicrectory)
     data.class_is_last()
@@ -43,12 +41,12 @@ def j48(dicrectory,nameOfDataSet,flag):
     if flag:
         eval.crossvalidate_model(cls, data, 10, Random(1), pout)
     else:
-        eval.evaluate_train_test_split(cls,data,80.0,Random(1),pout)
+        eval.evaluate_train_test_split(cls, data, 80.0, Random(1), pout)
     print_and_save('J48 model', flag, nameOfDataSet, eval)
     gc.collect()
 
 
-def naive_bayse(dicrectory,nameOfDataSet,flag):
+def naive_bayse(dicrectory, nameOfDataSet, flag):
     loader = Loader(classname="weka.core.converters.CSVLoader")
     data = loader.load_file(dicrectory)
     data.class_is_last()
@@ -58,40 +56,40 @@ def naive_bayse(dicrectory,nameOfDataSet,flag):
     if flag:
         eval.crossvalidate_model(cls, data, 10, Random(1), pout)
     else:
-        eval.evaluate_train_test_split(cls,data,80.0,Random(1),pout)
-    print_and_save('Naïve Bayes model', flag, nameOfDataSet, eval)
+        eval.evaluate_train_test_split(cls, data, 80.0, Random(1), pout)
+    print_and_save('Naive Bayes model', flag, nameOfDataSet, eval)
     gc.collect()
 
 
-def random_tree(dicrectory,nameOfDataSet,flag):
+def random_tree(dicrectory, nameOfDataSet, flag):
     loader = Loader(classname="weka.core.converters.CSVLoader")
     data = loader.load_file(dicrectory)
     data.class_is_last()
-    cls = Classifier(classname='weka.classifiers.trees.RandomTree', options=['-K','6'])
+    cls = Classifier(classname='weka.classifiers.trees.RandomTree', options=['-K', '6'])
     eval = Evaluation(data)
     pout = PredictionOutput(classname="weka.classifiers.evaluation.output.prediction.PlainText")
     if flag:
         eval.crossvalidate_model(cls, data, 10, Random(1), pout)
     else:
-        eval.evaluate_train_test_split(cls,data,80.0,Random(1),pout)
+        eval.evaluate_train_test_split(cls, data, 80.0, Random(1), pout)
     print_and_save('random tree model', flag, nameOfDataSet, eval)
     gc.collect()
 
 
-def print_and_save(modelname,flag,nameOfDataSet,eval):
+def print_and_save(modelname, flag, nameOfDataSet, eval):
     if flag:
-        file=open('./result/result_kfold.txt','a')
+        file = open('./result/result_kfold.txt', 'a')
     else:
-        file=open('./result/result_split_1run.txt','a')
+        file = open('./result/result_split_1run.txt', 'a')
     print('*************************************************')
     file.write('*************************************************')
     print('\n')
     file.write('\n')
-    print('the result of '+modelname+' on:'+nameOfDataSet)
-    file.write('the result of '+modelname+' on:'+nameOfDataSet)
+    print('the result of ' + modelname + ' on:' + nameOfDataSet)
+    file.write('the result of ' + modelname + ' on:' + nameOfDataSet)
     file.write('\n')
-    print('\*************************************************/')
-    file.write('\*************************************************/')
+    print('*************************************************/')
+    file.write('*************************************************/')
     file.write('\n')
     print(eval.percent_correct)
     file.write(str(eval.percent_correct))
@@ -114,33 +112,38 @@ def print_and_save(modelname,flag,nameOfDataSet,eval):
     file.write('\n')
     gc.collect()
 
-vote_classifier_train('./data/final/bolean_for_weka.csv','boolean_target',True)
-vote_classifier_train('./data/final/bolean_for_weka.csv','boolean_target',False)
-j48('./data/final/bolean_for_weka.csv','boolean_target',True)
-j48('./data/final/bolean_for_weka.csv','boolean_target',False)
-naive_bayse('./data/final/bolean_for_weka.csv','boolean_target',True)
-naive_bayse('./data/final/bolean_for_weka.csv','boolean_target',False)
-random_tree('./data/final/bolean_for_weka.csv','boolean_target',True)
-random_tree('./data/final/bolean_for_weka.csv','boolean_target',False)
+
+def main():
+    jvm.start()
+    vote_classifier_train('./data/final/bolean_for_weka.csv', 'boolean_target', True)
+    vote_classifier_train('./data/final/bolean_for_weka.csv', 'boolean_target', False)
+    j48('./data/final/bolean_for_weka.csv', 'boolean_target', True)
+    j48('./data/final/bolean_for_weka.csv', 'boolean_target', False)
+    naive_bayse('./data/final/bolean_for_weka.csv', 'boolean_target', True)
+    naive_bayse('./data/final/bolean_for_weka.csv', 'boolean_target', False)
+    random_tree('./data/final/bolean_for_weka.csv', 'boolean_target', True)
+    random_tree('./data/final/bolean_for_weka.csv', 'boolean_target', False)
+
+    vote_classifier_train(
+        './data/final/20 Percent Training Set reducedAttacks_data feature selected with normalized data.csv',
+        'reduced attacks to 4', True)
+    vote_classifier_train(
+        './data/final/20 Percent Training Set reducedAttacks_data feature selected with normalized data.csv',
+        'reduced attacks to 4', False)
+    j48('./data/final/20 Percent Training Set reducedAttacks_data feature selected with normalized data.csv',
+        'reduced attacks to 4', True)
+    j48('./data/final/20 Percent Training Set reducedAttacks_data feature selected with normalized data.csv',
+        'reduced attacks to 4', False)
+    naive_bayse('./data/final/20 Percent Training Set reducedAttacks_data feature selected with normalized data.csv',
+                'reduced attacks to 4', True)
+    naive_bayse('./data/final/20 Percent Training Set reducedAttacks_data feature selected with normalized data.csv',
+                'reduced attacks to 4', False)
+    random_tree('./data/final/20 Percent Training Set reducedAttacks_data feature selected with normalized data.csv',
+                'reduced attacks to 4', True)
+    random_tree('./data/final/20 Percent Training Set reducedAttacks_data feature selected with normalized data.csv',
+                'reduced attacks to 4', False)
+    jvm.stop()
 
 
-vote_classifier_train('./data/final/20 Percent Training Set reducedAttacks_data feature selected with normalized data.csv',
-                      'reduced attacks to 4',True)
-vote_classifier_train('./data/final/20 Percent Training Set reducedAttacks_data feature selected with normalized data.csv',
-                      'reduced attacks to 4',False)
-j48('./data/final/20 Percent Training Set reducedAttacks_data feature selected with normalized data.csv',
-    'reduced attacks to 4',True)
-j48('./data/final/20 Percent Training Set reducedAttacks_data feature selected with normalized data.csv',
-    'reduced attacks to 4',False)
-naive_bayse('./data/final/20 Percent Training Set reducedAttacks_data feature selected with normalized data.csv',
-            'reduced attacks to 4',True)
-naive_bayse('./data/final/20 Percent Training Set reducedAttacks_data feature selected with normalized data.csv',
-            'reduced attacks to 4',False)
-random_tree('./data/final/20 Percent Training Set reducedAttacks_data feature selected with normalized data.csv',
-            'reduced attacks to 4',True)
-random_tree('./data/final/20 Percent Training Set reducedAttacks_data feature selected with normalized data.csv',
-            'reduced attacks to 4',False)
-
-
-
-jvm.stop()
+if __name__ == '__main__':
+    main()
--- pre_process.py
+++ pre_process.py
@@ -1,80 +1,97 @@
 import pandas as pd
-from sklearn.feature_selection import mutual_info_regression,mutual_info_classif,mutual_info_
+from sklearn.feature_selection import mutual_info_regression
 import numpy as np
 from sklearn import preprocessing
 
 
-
-#
 def encode_for_weka():
-    data=pd.read_csv('./data/final/20 percent train set boolean feature selected with normalized data.csv')
-    weka_type_encode={0:False,1:True,2:True,3:True,4:True}
-    data['target'].replace(weka_type_encode,inplace=True)
+    data = pd.read_csv('./data/final/20 percent train set boolean feature selected with normalized data.csv')
+    weka_type_encode = {0: False, 1: True, 2: True, 3: True, 4: True}
+    data['target'].replace(weka_type_encode, inplace=True)
     data.to_csv('./data/final/bolean_for_weka.csv', sep=',', encoding='utf-8', index=False, header=True)
 
+
 def encode_data(filename):
-    train_data = pd.read_csv(filename+'.csv', header=-1)
-    train_data[1]=train_data[1].astype('category').cat.codes
-    train_data[2]=train_data[2].astype('category').cat.codes
-    train_data[3]=train_data[3].astype('category').cat.codes
-    reduced_attacks={"normal": "normal","neptune":"dos", "warezclient":"r2l", "ipsweep":"probe", "portsweep":"probe", "teardrop":"dos", "nmap":"probe",
-     "satan":"probe", "smurf":"dos", "pod":"dos", "back":"dos","guess_passwd":"r2l","ftp_write":"r2l","multihop":"r2l","rootkit":"u2r","buffer_overflow":"u2r","imap":"r2l","warezmaster":"r2l","phf":"r2l","land":"dos",
-                     "loadmodule":"u2r","spy":"r2l"}
-    train_data[41].replace(reduced_attacks,inplace=True)
-    train_data.to_csv(filename+' reducedAttacks_data.csv',sep=',', encoding='utf-8',index=False,header=False)
-    attck_encode={"normal":0, "dos":1 ,"r2l":2, "probe":3 ,"u2r":4}
-    train_data[41].replace(attck_encode,inplace=True)
-    train_data.to_csv(filename+' encoded_data.csv',sep=',', encoding='utf-8',index=False,header=False)
-    two_type_encode={0:0,1:1,2:1,3:1,4:1}
-    train_data[41].replace(two_type_encode,inplace=True)
-    train_data.to_csv(filename+' bolean_attack.csv',sep=',', encoding='utf-8',index=False,header=False)
+    train_data = pd.read_csv(filename + '.csv', header=-1)
+    train_data[1] = train_data[1].astype('category').cat.codes
+    train_data[2] = train_data[2].astype('category').cat.codes
+    train_data[3] = train_data[3].astype('category').cat.codes
+    reduced_attacks = {"normal": "normal", "neptune": "dos",
+                       "warezclient": "r2l", "ipsweep": "probe",
+                       "portsweep": "probe", "teardrop": "dos",
+                       "nmap": "probe", "satan": "probe",
+                       "smurf": "dos", "pod": "dos",
+                       "back": "dos", "guess_passwd": "r2l",
+                       "ftp_write": "r2l", "multihop": "r2l",
+                       "rootkit": "u2r", "buffer_overflow": "u2r",
+                       "imap": "r2l", "warezmaster": "r2l",
+                       "phf": "r2l", "land": "dos",
+                       "loadmodule": "u2r", "spy": "r2l"}
+    train_data[41].replace(reduced_attacks, inplace=True)
+    train_data.to_csv(filename + ' reducedAttacks_data.csv', sep=',',
+                      encoding='utf-8', index=False, header=False)
+    attck_encode = {"normal": 0, "dos": 1, "r2l": 2, "probe": 3, "u2r": 4}
+    train_data[41].replace(attck_encode, inplace=True)
+    train_data.to_csv(filename + ' encoded_data.csv', sep=',', encoding='utf-8',
+                      index=False, header=False)
+    two_type_encode = {0: 0, 1: 1, 2: 1, 3: 1, 4: 1}
+    train_data[41].replace(two_type_encode, inplace=True)
+    train_data.to_csv(filename + ' bolean_attack.csv', sep=',', encoding='utf-8',
+                      index=False, header=False)
 
 
-def slected_feature_data_sets_save(ind,filename):
-    train_dat=pd.read_csv(filename+'.csv',header=-1)
-    target_df=train_dat[41]
-    train_dat=train_dat.drop(ind,axis=1)
-    train_dat=train_dat.drop(42,axis=1)
-    train_dat=pd.concat([train_dat,target_df],axis=1)
-    train_dat.to_csv(filename + ' feature selected.csv',sep=',', encoding='utf-8',index=False,header=False)
+def slected_feature_data_sets_save(ind, filename):
+    train_dat = pd.read_csv(filename + '.csv', header=-1)
+    target_df = train_dat[41]
+    train_dat = train_dat.drop(ind, axis=1)
+    train_dat = train_dat.drop(42, axis=1)
+    train_dat = pd.concat([train_dat, target_df], axis=1)
+    train_dat.to_csv(filename + ' feature selected.csv', sep=',', encoding='utf-8',
+                     index=False, header=False)
 
 
 def find_features():
-    train_dat=pd.read_csv('./data/20 Percent Training Set bolean_attack.csv',header=-1)
-    train=np.asarray(train_dat)
-    target=train[:,41]
-    train=np.delete(train,41,axis=1)
-    mi=mutual_info_regression(train,target)
-    ind=np.argsort(mi,axis=0)
-    target_df=train_dat[41]
+    train_dat = pd.read_csv('./data/20 Percent Training Set bolean_attack.csv', header=-1)
+    train = np.asarray(train_dat)
+    target = train[:, 41]
+    train = np.delete(train, 41, axis=1)
+    mi = mutual_info_regression(train, target)
+    ind = np.argsort(mi, axis=0)
+    target_df = train_dat[41]
     print(ind[ind.__len__() - 8:ind.__len__()])
-    ind=ind[0:ind.__len__()-8]
-    train_dat=train_dat.drop(ind,axis=1)
-    train_dat=train_dat.drop(42,axis=1)
-    train_dat=pd.concat([train_dat,target_df],axis=1)
-    train_dat.to_csv('./data/20 percent train set boolean feature selected.csv',sep=',', encoding='utf-8',index=False,header=False)
-    slected_feature_data_sets_save(ind,'./data/20 Percent Training Set encoded_data')
-    slected_feature_data_sets_save(ind,'./data/20 Percent Training Set reducedAttacks_data')
+    ind = ind[0:ind.__len__() - 8]
+    train_dat = train_dat.drop(ind, axis=1)
+    train_dat = train_dat.drop(42, axis=1)
+    train_dat = pd.concat([train_dat, target_df], axis=1)
+    train_dat.to_csv('./data/20 percent train set boolean feature selected.csv', sep=',', encoding='utf-8',
+                     index=False, header=False)
+    slected_feature_data_sets_save(ind, './data/20 Percent Training Set encoded_data')
+    slected_feature_data_sets_save(ind, './data/20 Percent Training Set reducedAttacks_data')
+
+
 def normalized_data(filename):
+    data = pd.read_csv('./data/' + filename + '.csv', header=-1)
+    target = data[8]
+    data = data.drop(8, axis=1)
+    x = data.values
+    min_max_scaler = preprocessing.MinMaxScaler()
+    x_scale = min_max_scaler.fit_transform(x)
+    data = pd.DataFrame(x_scale)
+    data = pd.concat([data, target], axis=1)
+    data.columns = ["service", "flag", "src bytes", "dst bytes", "same srv rate",
+                    "diff srv rate", "dst host srv count", "dst host same srv rate", "target"]
+    data.to_csv('./data/final/' + filename + ' with normalized data' + '.csv', sep=',', encoding='utf-8',
+                index=False, header=True)
 
-    data=pd.read_csv('./data/'+filename+'.csv',header=-1)
-    target=data[8]
-    data=data.drop(8,axis=1)
-    x=data.values
-    min_max_scaler=preprocessing.MinMaxScaler()
-    x_scale=min_max_scaler.fit_transform(x)
-    data=pd.DataFrame(x_scale)
-    data=pd.concat([data,target],axis=1)
-    data.columns=["service", "flag","src bytes","dst bytes","same srv rate","diff srv rate"
-        ,"dst host srv count","dst host same srv rate","target"]
-    data.to_csv('./data/final/'+filename+' with normalized data'+'.csv',sep=',', encoding='utf-8',index=False,header=True)
+
+def main():
+    encode_data('./data/20 Percent Training Set')
+    find_features()
+    normalized_data('20 Percent Training Set reducedAttacks_data feature selected')
+    normalized_data('20 Percent Training Set encoded_data feature selected')
+    normalized_data('20 percent train set boolean feature selected')
+    encode_for_weka()
 
 
-
-encode_data('./data/20 Percent Training Set')
-find_features()
-
-normalized_data('20 Percent Training Set reducedAttacks_data feature selected')
-normalized_data('20 Percent Training Set encoded_data feature selected')
-normalized_data('20 percent train set boolean feature selected')
-encode_for_weka()
+if __name__ == '__main__':
+    main()
